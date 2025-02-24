7ae42c2aa6434e86922ba6872c93be0dafb3f970
Sumedh1119 <sumedh.bharadwajh@gmail.com>
2025-02-24 09:38:35 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Sumedh Bharadwaj`;
 export const OWNER_DESCRIPTION: string = `A first year MBA student at UNC Kenan Flagler`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Nova`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
