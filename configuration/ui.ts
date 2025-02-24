47a0879f8ad980c53a865b243b9b620f410b6346
Sumedh1119 <sumedh.bharadwajh@gmail.com>
2025-02-24 16:34:25 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Sumedh's AI Bot`;
+export const CHAT_HEADER: string = `Nova: Your guide to KFBS`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Sumedh Bharadwaj Copyright. 2025`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.Ask me about navigating life as a first year MBA student at KFBS`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
